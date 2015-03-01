2075a3841c851f4ffc9f2c9953f3796ce36b0228
Samvel Stepanyan <samvelst@gmail.com>
2015-02-28 22:24:48 -0800

    Fixed bugs and added support for showing a schedule. We can now schedule shifts for volunteers with each having the same available hours.

--- src/sswing_scheduler.py
+++ src/sswing_scheduler.py
@@ -28,15 +28,22 @@ class Volunteer:
 
     def can_take_job(self, a_job):
         if self.current_capacity + a_job.length > self.capacity:
+            print "%s will put %s over capacity" % (a_job, self.name)
             return False
         elif len(self.jobs) == 0:
             return True
         else:
             for j in self.jobs:
-                if j.start < a_job.start < j.end or j.start < a_job.end < j.end:
+                if a_job.conflicts_with(j):
                     return False
-                else:
-                    return True
+            return True
+
+    def print_schedule(self):
+        self.jobs = sorted(self.jobs, key=lambda x: x.start)
+        print "Name: %s" % self.name
+        for j in self.jobs:
+            print "\t%s :: %s - %s" % (j.name, j.start, j.end)
+
 
 class Job:
     def __init__(self, a_name, a_time_interval):
@@ -50,42 +57,52 @@ class Job:
     def __repr__(self):
         return "%s :: %s - %s" % (self.name, self.start, self.end)
 
+    def conflicts_with(self, another_job):
+        if another_job.start == self.start or another_job.end == self.end:
+            return True
+        elif (self.start < another_job.start < self.end) or (self.start < another_job.end < self.end):
+            return True
+        else:
+            return False
+
 
 # test code
 
 def rand_time_interval(a_min, a_max):
     a = random.randint(a_min, a_max-1)
-    b = random.randint(a+1, a_max)
-    return (a, b)
+    # b = random.randint(a+1, a_max)
+    return (a, a+1)
 
 def show_schedule(volunteers):
     for v in volunteers:
-        if v.current_capacity != 0:
-            print "Name: %s" % v.name
-            for j in v.jobs:
-                print "\t%s :: %s - %s" % (j.name, j.start, j.end)
+        v.print_schedule()
 
 V = [SS.Volunteer(a_name="Volunteer " + str(z)) for z in xrange(1,11)]
-J = sorted([SS.Job("Job "+str(z), rand_time_interval(0,4)) for z in xrange(1,11)])
+J = sorted([SS.Job("Job "+str(z), rand_time_interval(0,4)) for z in xrange(1,21)])
 
 
 # algorithm (greedy)
-
+db_count = 1
 while len(J) > 0:
+    print "Current loop %s" % db_count
     j = J.pop(0)
-    print j
+    print "Assigning: %s" % j
     curr_job_assigned = False
     V = sorted(V)
 
     for v in V:
+        print "Considering..."
+        v.print_schedule()
         if v.can_take_job(j):
-            print v
             v.add_job(j)
+            print "Assigning job to: %s" % v
             curr_job_assigned = True
             break
 
     if not curr_job_assigned:
         print "Error: could not find assignment for %s" % j
         break
+
+    db_count += 1
 
 show_schedule(V)
