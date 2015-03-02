75f3e49397c27086714c44a80f9285a335d4b237
Samvel Stepanyan <samvelst@gmail.com>
2015-03-02 01:22:16 -0800

    Refactored and made getting output much nicer. Ctrl-C to terminate and get best current solution.

---
+++ src/RandomizedBFD.py
@@ -0,0 +1,103 @@
+import sys
+import random
+import copy
+import Work as SS
+
+
+def rand_time_interval(a_min, a_max, a_length):
+    start = random.randint(a_min, a_max - a_length)
+    return (start, start+a_length)
+
+
+def show_schedule(volunteers):
+    for v in volunteers:
+        if v.is_used:
+            v.print_schedule()
+
+
+def make_random_volunteers(num_of_volunteers, hour_range):
+    V = []
+    for x in xrange(1, num_of_volunteers+1):
+        name = "Volunteer " + str(x)
+        capacity = random.randint(hour_range[0], hour_range[1])
+        volunteer = SS.Volunteer(name, capacity)
+        available_start = random.randint(0,18)
+        volunteer.add_job(SS.JobShift("UNAVAILABLE", (0, available_start)))
+        volunteer.add_job(SS.JobShift("UNAVAILABLE", (available_start+capacity, 24)))
+        V.append(volunteer)
+    return V
+
+
+def make_random_jobs(num_of_jobs):
+    J = []
+    for y in xrange(1, 31):
+        name = "Shift " + str(y)
+        time_interval = rand_time_interval(0, 21, random.randint(1,3))
+        job = SS.JobShift(name, time_interval)
+        J.append(job)
+    return J
+
+
+def assign_jobs(jobs, volunteers):
+    unassigned_jobs = []
+    current_jobs = sorted(jobs)
+    current_volunteers = volunteers
+
+    while len(current_jobs) > 0:
+        job = current_jobs.pop(0)
+        current_volunteers = sorted(current_volunteers)
+        job_assigned = False
+
+        for volunteer in current_volunteers:
+            if volunteer.can_take_job(job):
+                volunteer.add_job(job)
+                job_assigned = True
+                break
+
+        if not job_assigned:
+            unassigned_jobs.append(job)
+
+    return (current_volunteers, unassigned_jobs)
+
+
+volunteers = make_random_volunteers(60, (2,6))
+jobs = make_random_jobs(30)
+
+total_job_hours = sum([j.length for j in jobs])
+avg_vol_capacity = sum([v.capacity for v in volunteers])/float(60)
+estimated_lower_bound = int((total_job_hours/avg_vol_capacity)+1)
+
+current_best_schedule = []
+min_volunteers_needed = 60
+
+print "Estimated Lower Bound: %s..." % estimated_lower_bound
+while(min_volunteers_needed > estimated_lower_bound):
+    try:
+        volunteers, unassigned_jobs = assign_jobs(jobs, volunteers)
+
+        if len(unassigned_jobs) == 0:
+            current_min = sum([1 for x in volunteers if x.is_used])
+            if current_min < min_volunteers_needed:
+                min_volunteers_needed = current_min
+                current_best_schedule = copy.deepcopy(volunteers)
+                print "Current min volunteers needed: %s" % min_volunteers_needed
+
+        map(lambda x: x.clear_all(), volunteers)
+        random.shuffle(volunteers)
+
+    except KeyboardInterrupt:
+        print "Terminating...\n\n"
+        if len(unassigned_jobs) == 0:
+            print "Min # of volunteers found so far: %s" % min_volunteers_needed
+            print "Schedule: "
+            show_schedule(current_best_schedule)
+            print "\n"
+        else:
+            print "Not able to find feasible solution"
+            print "Unassigned jobs: %s" % unassigned_jobs
+            print "Current best schedule: "
+            show_schedule(volunteers)
+        sys.exit()
+
+print "Finished with optimal (%s) number of volunteers." % min_volunteers_needed
+show_schedule(current_best_schedule)
---
+++ src/Work.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+#
+# Work objects
+#
+
+
+class Volunteer:
+    """
+    Represents a single volunteer
+    String name - Name of the volunteer
+    Int capacity - How many hours this volunteer has available
+    Int current_capacity - How many available hours the volunteer current has
+    [Job] jobs - Current list of jobs assigned to volunteer
+    Boolean is_used - True if the volunteer's job list is not empty
+    """
+    def __init__(self, a_name="", a_capacity=4):
+        self.name = a_name
+        self.capacity = a_capacity
+        self.current_capacity = 0
+        self.jobs = []
+        self.is_used = False
+
+    def __cmp__(self, other):
+        return -cmp(self.current_capacity, other.current_capacity)
+
+    def __repr__(self):
+        return self.name
+
+    def add_job(self, a_job):
+        self.jobs.append(a_job)
+        if not a_job.name.startswith("UNAVAILABLE"):
+            self.current_capacity += a_job.length
+            self.is_used = True
+
+    def can_take_job(self, a_job):
+        for current_job in self.jobs:
+            if a_job.conflicts_with(current_job):
+                return False
+
+        if (self.current_capacity + a_job.length) > self.capacity:
+            return False
+
+        return True
+
+    def print_schedule(self):
+        self.jobs = sorted(self.jobs, key=lambda x: x.start)
+        print "Name: %s :: Hours %s" % (self.name, self.capacity)
+        for j in self.jobs:
+            print "\t%s :: %s - %s" % (j.name, j.start, j.end)
+
+    def clear_all(self):
+        self.jobs = filter(lambda x: x.name.startswith('UNAVAILABLE'), self.jobs)
+        self.current_capacity = 0
+        self.is_used = False
+
+
+class JobShift:
+    """
+    Represents a single shift
+    String name - name of the shift
+    Int start - shift's start time
+    Int end - shift's end time
+    [Int] interval - all hours covered by this shift
+    Int length - total number of hours for this shift
+    """
+    def __init__(self, a_name, a_time_interval):
+        self.name = a_name
+        self.start, self.end  = a_time_interval
+        self.interval = set(range(self.start, self.end+1))
+        self.length = self.end - self.start
+
+    def __cmp__(self, other):
+        return -cmp(self.length, other.length)
+
+    def __repr__(self):
+        return "%s :: %s - %s" % (self.name, self.start, self.end)
+
+    def conflicts_with(self, another_job):
+        return len(self.interval & another_job.interval) > 1
